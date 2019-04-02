6590251f12a97b759e81c39a7804c964e28f0839
Brian Branch <reisuj@gmail.com>
2019-04-02 15:11:07 -0400

    Add project files.

---
+++ Digital_Clock/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Digital_Clock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblTime = new System.Windows.Forms.Label();
+            this.tmrClock = new System.Windows.Forms.Timer(this.components);
+            this.btnBreak = new System.Windows.Forms.Button();
+            this.btnLunch = new System.Windows.Forms.Button();
+            this.lblCountDown = new System.Windows.Forms.Label();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.tmrCountDown = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTime.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblTime.Location = new System.Drawing.Point(26, 7);
+            this.lblTime.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(174, 46);
+            this.lblTime.TabIndex = 0;
+            this.lblTime.Text = "00:00:00";
+            this.lblTime.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // tmrClock
+            // 
+            this.tmrClock.Enabled = true;
+            this.tmrClock.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnBreak
+            // 
+            this.btnBreak.Location = new System.Drawing.Point(7, 56);
+            this.btnBreak.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnBreak.Name = "btnBreak";
+            this.btnBreak.Size = new System.Drawing.Size(56, 19);
+            this.btnBreak.TabIndex = 1;
+            this.btnBreak.Text = "Break";
+            this.btnBreak.UseVisualStyleBackColor = true;
+            this.btnBreak.Click += new System.EventHandler(this.btnBreak_Click);
+            // 
+            // btnLunch
+            // 
+            this.btnLunch.Location = new System.Drawing.Point(7, 79);
+            this.btnLunch.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnLunch.Name = "btnLunch";
+            this.btnLunch.Size = new System.Drawing.Size(56, 19);
+            this.btnLunch.TabIndex = 2;
+            this.btnLunch.Text = "Lunch";
+            this.btnLunch.UseVisualStyleBackColor = true;
+            this.btnLunch.Click += new System.EventHandler(this.btnLunch_Click);
+            // 
+            // lblCountDown
+            // 
+            this.lblCountDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCountDown.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblCountDown.Location = new System.Drawing.Point(129, 56);
+            this.lblCountDown.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblCountDown.Name = "lblCountDown";
+            this.lblCountDown.Size = new System.Drawing.Size(84, 42);
+            this.lblCountDown.TabIndex = 3;
+            this.lblCountDown.Text = "00:00:00";
+            this.lblCountDown.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnPause
+            // 
+            this.btnPause.Location = new System.Drawing.Point(68, 56);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(56, 19);
+            this.btnPause.TabIndex = 4;
+            this.btnPause.Text = "Pause";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(68, 79);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(56, 19);
+            this.btnReset.TabIndex = 5;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // tmrCountDown
+            // 
+            this.tmrCountDown.Interval = 1000;
+            this.tmrCountDown.Tick += new System.EventHandler(this.tmrCountDown_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(224, 114);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.lblCountDown);
+            this.Controls.Add(this.btnLunch);
+            this.Controls.Add(this.btnBreak);
+            this.Controls.Add(this.lblTime);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "BreakMinder";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Timer tmrClock;
+        private System.Windows.Forms.Button btnBreak;
+        private System.Windows.Forms.Button btnLunch;
+        private System.Windows.Forms.Label lblCountDown;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Timer tmrCountDown;
+    }
+}
+
---
+++ Digital_Clock/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Digital_Clock
+{
+    public partial class Form1 : Form
+    {
+        int totalSeconds;
+
+        bool isPaused = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblTime.Text = DateTime.Now.ToString("HH:mm:ss");
+        }
+
+        
+
+        private void tmrCountDown_Tick(object sender, EventArgs e)
+        {
+            totalSeconds--;
+            lblCountDown.Text = "00:" + (totalSeconds / 60).ToString("00") + ":" + (totalSeconds % 60).ToString("00");
+            if (totalSeconds <= 0)
+            {
+                tmrCountDown.Enabled = false;
+                MessageBox.Show("Time to get back to Work");                
+                btnReset.PerformClick();
+            }
+        }
+
+        private void btnBreak_Click(object sender, EventArgs e)
+        {
+            CountDown(900, "00:15:00");
+        }
+
+        private void btnLunch_Click(object sender, EventArgs e)
+        {
+            CountDown(3600, "01:00:00");
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            if (!isPaused)
+            {
+                tmrCountDown.Enabled = false;
+                btnPause.Text = "Start";
+                isPaused = true;
+            }
+            else
+            {
+                tmrCountDown.Enabled = true;
+                btnPause.Text = "Pause";
+                isPaused = false;
+            }            
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            tmrCountDown.Enabled = false;
+            isPaused = false;
+            btnPause.Text = "Pause";
+            lblCountDown.Text = "00:00:00";
+        }
+
+        // Method to initiate the countdown
+        private void CountDown(int seconds, string startTime)
+        {
+            totalSeconds = seconds;
+            lblCountDown.Text = startTime;
+            tmrCountDown.Enabled = true;
+        }
+    }
+}
